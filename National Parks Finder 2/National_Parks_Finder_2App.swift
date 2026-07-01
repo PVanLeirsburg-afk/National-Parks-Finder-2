654bc7516bf79cbba044f11959716e6cf27debb8
Pam VanLeirsburg <pvanleirsburg@barrington220.org>
2026-07-01 13:37:40 -0500

    Initial Commit

---
+++ National Parks Finder 2/National_Parks_Finder_2App.swift
@@ -0,0 +1,17 @@
+//
+//  National_Parks_Finder_2App.swift
+//  National Parks Finder 2
+//
+//  Created by Pamela VanLeirsburg on 7/1/26.
+//
+
+import SwiftUI
+
+@main
+struct National_Parks_Finder_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
