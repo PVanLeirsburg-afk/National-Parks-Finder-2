afb4dc6ce4e17aed41c40bdadfd58a0744e7dd74
Pam VanLeirsburg <pvanleirsburg@barrington220.org>
2026-07-01 15:53:30 -0500

    All DONE

---
+++ National Parks Finder 2/ParkLocationView.swift
@@ -0,0 +1,18 @@
+//
+//  ParkLocationView.swift
+//  National Parks Finder 2
+//
+//  Created by Pamela VanLeirsburg on 7/1/26.
+//
+
+import SwiftUI
+
+struct ParkLocationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ParkLocationView()
+}
---
+++ National Parks Finder 2/ParkMapView.swift
@@ -0,0 +1,8 @@
+//
+//  ParkMapView.swift
+//  National Parks Finder 2
+//
+//  Created by Pamela VanLeirsburg on 7/1/26.
+//
+
+import Foundation
